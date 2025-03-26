e5cff345bbffac4afdd3e41600caee1cd4db6f7a
dcthanh <thanh261103@gmail.com>
2025-03-27 01:59:25 +0700

    DEPLOY

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // 🔥 Cố định port 5173
-    strictPort: true, // Không tự động đổi port nếu 5173 đang bị chiếm
+    historyApiFallback: true, // Để hỗ trợ React Router
   },
-})
+});
