a044923f9b565f367be916feb6e7569d3d20f68c
dcthanh <thanh261103@gmail.com>
2025-03-27 02:03:46 +0700

    DEPLOY

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Đảm bảo đường dẫn tương đối
   server: {
-    historyApiFallback: true, // Để hỗ trợ React Router
+    historyApiFallback: true, // Hỗ trợ React Router
+  },
+  build: {
+    outDir: "dist", // Đảm bảo thư mục build đúng
   },
 });
